cf6c8d097bd1b6d522b0dccd770d5401f3cac33b
ArgotePazRamiro <argote_paz_ramiro@hotmail.com>
2022-12-10 02:03:57 -0400

    admin login y rutas de deslogueo

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
@@ -11,5 +12,21 @@ class AdminController extends Controller
 
         return view('admin.index');
 
+    }
+
+    public function AdminLogin()
+    {
+        return view('admin.admin_login');
+    }
+
+    public function AdminDestroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/admin/login');
     }
 }
