4bd7c3dbec07fe37b6c0e3044891874fbf6d0310
ArgotePazRamiro <argote_paz_ramiro@hotmail.com>
2022-12-09 22:41:05 -0400

    logeo y proteccion de urls

---
+++ resources/views/admin/admin_dashboard.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Panel de Administrador</title>
+</head>
+<body>
+    <h1>
+        Panel de Administrador
+    </h1>
+</body>
+</html>
---
+++ resources/views/vendor/vendor_dashboard.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Panel de Vendedor</title>
+</head>
+<body>
+    <h1>
+        Panel de Vendedor
+    </h1>
+</body>
+</html>
