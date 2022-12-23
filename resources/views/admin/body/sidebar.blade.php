f4652c8193bf25558cabbaf2cc3da6c9de49e42f
ArgotePazRamiro <argote_paz_ramiro@hotmail.com>
2022-12-23 03:16:41 -0400

    CRUD sliders backend

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -76,6 +76,22 @@
                
             </ul>
         </li>
+
+        <li>
+            <a href="javascript:;" class="has-arrow">
+                <div class="parent-icon"><i class="bx bx-category"></i>
+                </div>
+                <div class="menu-title">Manejo de Slider</div>
+            </a>
+            <ul>
+                <li> <a href="{{ route('all.slider') }}"><i class="bx bx-right-arrow-alt"></i>Slider Disponibles</a>
+                </li>
+                <li> <a href="{{ route('add.slider') }}"><i class="bx bx-right-arrow-alt"></i>Añadir Slider</a>
+                </li>
+               
+            </ul>
+        </li>
+
         <li class="menu-label">UI Elements</li>
         
         <li>
--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,8 @@ use App\Http\Controllers\Backend\CategoryController;
 use App\Http\Controllers\Backend\SubCategoryController;
 use App\Http\Controllers\Backend\ProductController;
 use App\Http\Controllers\Backend\VendorProductController;
+use App\Http\Controllers\Backend\SliderController;
+
 use App\Http\Middleware\RedirectIfAuthenticated;
 
 /*
@@ -161,7 +163,20 @@ Route::middleware(['auth', 'role:admin'])->group(function () {
         
     });
 
-});//End Middleware
+    //Slider All Route
+    Route::controller(SliderController::class)->group(function () {
+        Route::get('/all/slider', 'AllSlider')->name('all.slider');
+        Route::get('/add/slider', 'AddSlider')->name('add.slider');
+        Route::post('/store/slider', 'StoreSlider')->name('store.slider');
+        Route::get('/edit/slider/{id}', 'EditSlider')->name('edit.slider');
+        Route::post('/update/slider', 'UpdateSlider')->name('update.slider');
+        Route::get('/delete/slider/{id}', 'DeleteSlider')->name('delete.slider');
+
+    });
+
+
+
+});//Admin Middleware End
 
 
 
