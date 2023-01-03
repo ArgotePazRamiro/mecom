6db4fbae086a93f66149dba34d6221b26cb85ce0
ArgotePazRamiro <argote_paz_ramiro@hotmail.com>
2023-01-02 22:19:20 -0400

    quickview de productos

--- routes/web.php
+++ routes/web.php
@@ -202,6 +202,10 @@ Route::get('/vendor/all', [IndexController::class, 'VendorAll'])->name('vendor.a
 Route::get('product/category/{id}/{slug}', [IndexController::class, 'CatWiseProduct']);
 Route::get('product/subcategory/{id}/{slug}', [IndexController::class, 'SubCatWiseProduct']);
 
+//Product View Modal with Ajax
+
+Route::get('product/view/modal/{id}', [IndexController::class, 'ProductViewAjax']);
+
 
 
 // // Route::get('/dashboard', function () {
